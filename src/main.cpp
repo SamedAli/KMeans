209d4a43e85dbe16e057b109e0e3eb9255073907
SamedAli <9093568+SamedAli@users.noreply.github.com>
2025-04-05 13:58:36 +0200

    make ready for parallelism

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,50 @@
 #include "kmeansFunctions.h"
 
+#include <chrono>
 #include <filesystem>
 #include <iostream>
 
-/*
-1 Decide how many clusters you want, i.e. choose k
-2 Randomly assign a centroid to each of the k clusters
-3 Calculate the distance of all observation to each of the k centroids
-4 Assign observations to the closest centroid
-5 Find the new location of the centroid by taking the mean of all the observations in each cluster
-6 Repeat steps 3-5 until the centroids do not change position
-*/
-auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int
+auto kmeansIteration(const std::uint16_t nofIterations, const std::vector<std::vector<double>> &datapoints, std::vector<std::vector<double>> &centroids, std::vector<std::uint16_t> &clusters) -> void
 {
-	const std::uint16_t nofClusters   = 6;
-	const std::uint16_t nofIterations = 10'000;
-
-	std::vector<std::uint16_t> dataPointsClusterNumber;
-	const std::string          filename_  = std::filesystem::path("assets") / "data2.csv";
-	const char                 delimiter_ = ',';
-
-	const auto dataPoints_ = readDataPoints(filename_, delimiter_);
-	auto       centroids_  = initializeCentroids(dataPoints_, nofClusters);
-
-	dataPointsClusterNumber.resize(dataPoints_.size());
+	auto start = std::chrono::high_resolution_clock::now();
 
 	for (std::size_t i = 0; i < nofIterations; ++i)
 	{
 		bool centroidsChanged_ = false;
 		std::cout << "# Iteration: " << i << "\n";
 
-		assignClusters(dataPoints_, centroids_, dataPointsClusterNumber);
-		auto newCentroids_ = recalculateCentroids(dataPoints_, dataPointsClusterNumber, centroids_);
+		assignClusters(datapoints, centroids, clusters);
+		auto newCentroids_ = recalculateCentroids(datapoints, clusters, centroids);
 
-		if (!(centroids_ == newCentroids_))
+		if (!(centroids == newCentroids_))
 		{
-			centroids_        = std::move(newCentroids_);
+			centroids         = std::move(newCentroids_);
 			centroidsChanged_ = true;
 		}
 
 		if (!centroidsChanged_)
 			break;
 	}
+
+	auto                          end      = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> duration = end - start;
+	std::cout << "# Execution time: " << duration.count() << " seconds\n";
+}
+
+auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int
+{
+	const std::uint16_t nofClusters_   = 6;
+	const std::uint16_t nofIterations_ = 10'000;
+
+	std::vector<std::uint16_t> dataPointsClusterNumber_;
+	const std::string          filename_  = std::filesystem::path("assets") / "data2.csv";
+	const char                 delimiter_ = ',';
+
+	const auto dataPoints_ = readDataPoints(filename_, delimiter_);
+	auto       centroids_  = initializeCentroids(dataPoints_, nofClusters_);
+	dataPointsClusterNumber_.resize(dataPoints_.size());
+
+	kmeansIteration(nofIterations_, dataPoints_, centroids_, dataPointsClusterNumber_);
 
 	std::cout << "# Writing to file.\n";
 	dumpToCSV(centroids_, std::filesystem::path("assets") / "centroids.csv");
